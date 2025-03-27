128725eca47949d852e1263b24a7d9526325c87e
Daniel Gomez <daniel.gomez37@alu.uclm.es>
2025-03-27 14:23:35 +0100

    Solucion asignacion no bloqueante

--- hw/cnt.sv
+++ hw/cnt.sv
@@ -35,6 +35,7 @@ module cnt #(
   logic         tc;  // terminal count
   logic [W-1:0] ciclos_nivel_alto;  
   logic [W-1:0] ciclos_nivel_bajo;
+  logic update_nivel_bajo;
 
   // La idea es modificar el contador, para que en lugar de hacer una interrupcion ala salida, genere una señal PWM configurable
   // Para no cambiar los nombres, se utilizara la señal tc_o para generar la señal PWM
@@ -47,9 +48,10 @@ module cnt #(
     else begin  // synchronous clear and count
       if (clr_i) begin
         // Por defecto, la señal PWM es de un periodo de 10 ciclos y un ciclo de trabajo de 0
-        ciclos_nivel_alto <= 9;
-        ciclos_nivel_bajo <= 0;
+        ciclos_nivel_alto <= 'hA;
+        ciclos_nivel_bajo <= 'h0;
         cnt_val <= 'h0;
+        update_nivel_bajo <= 0;
       end
       else if (ld_i) begin
         // Para facilitar el diseño, la señal thr_i es el numero de ciclos del periodo de la señal PWM
@@ -58,9 +60,14 @@ module cnt #(
         // thr_i = 10, la señal PWM tendra un periodo de 10 ciclos
         // ld_val_i = 50, la señal PWM tendra un ciclo de trabajo de 5 ciclos
         // Para hacer un redondeo, se utuliza un factor de 50 antes de dividir: 
-         // si thr_i = 4 y ld_val_i = 49: 4 * 49 / 100 = 1, pero si se aplica el redondeo:
-          // (4 * 49) + 50 / 100 = 2 
+        // si thr_i = 4 y ld_val_i = 49: 4 * 49 / 100 = 1, pero si se aplica el redondeo:
+        // (4 * 49) + 50 / 100 = 2 
+        /* Al ser asignaciuones no bloqueantes, es necesario crear un segundo ciclo para actualizar el valor dl nivel*/
         ciclos_nivel_alto <= (((ld_val_i * thr_i) + 50) / 100);
+        update_nivel_bajo <= 1;
+      end
+      else if (update_nivel_bajo) begin
+        update_nivel_bajo <= 0;
         ciclos_nivel_bajo <= thr_i - ciclos_nivel_alto;
       end
       else if (en_i) begin
